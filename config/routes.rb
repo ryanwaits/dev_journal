90cbe0ea90318003328d477bef662b67f7e7c6fb
Ryan Waits <ryan.waits@gmail.com>
2016-11-07 15:19:42 -0600

    nest parts resources inside tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/dashboard' => 'tasks#index', as: 'dashboard'
-  resources :tasks, except: [:index]
+  resources :tasks, except: [:index] do
+    resources :parts
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
