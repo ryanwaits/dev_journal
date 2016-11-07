a065c9bab0939e22ce4f30c2b38f1fd623bd34c9
Ryan Waits <ryan.waits@gmail.com>
2016-11-07 12:28:32 -0600

    resources for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/dashboard' => 'tasks#index', as: 'dashboard'
+  resources :tasks, except: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
