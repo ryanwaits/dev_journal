f669071ef5aa07cf669593758a6a42b7085a8a11
Ryan Waits <ryan.waits@gmail.com>
2016-11-07 15:25:49 -0600

    set up parts controller

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,2 +1,15 @@
 class PartsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def part_params
+    params.require(:part).permit(:description, :estimate, :actual, :reflection, :task_id)
+  end
+
 end
