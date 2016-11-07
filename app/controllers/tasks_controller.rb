2cfe4d67d54281ecd9935a62220acbdb527b76e7
Ryan Waits <ryan.waits@gmail.com>
2016-11-07 14:58:04 -0600

    update create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
   end
   
@@ -7,4 +9,26 @@ class TasksController < ApplicationController
   
   def new
   end
+
+  def create
+    @task = Task.create(task_params)
+    if @task.save
+      redirect_to dashboard_path
+      flash[:notice] = 'Task created!'
+    else
+      render :new
+      flash[:error] = 'Unable to create task'
+    end
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :level)
+  end
+
 end
