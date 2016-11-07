36df1689089756c056b49a121795b8f4e355c375
Ryan Waits <ryan.waits@gmail.com>
2016-11-07 14:57:56 -0600

    change default value of done to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107182518) do
+ActiveRecord::Schema.define(version: 20161107204645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20161107182518) do
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.integer  "level"
-    t.boolean  "done"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "done",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
