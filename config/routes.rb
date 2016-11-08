1b84e2c908670c3d9d559e9653c233c0649b9708
Ryan Waits <ryan.waits@gmail.com>
2016-11-07 18:28:27 -0600

    add routes for parts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :tasks, except: [:index] do
     resources :parts
   end
+  resources :parts, only: [:show, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
