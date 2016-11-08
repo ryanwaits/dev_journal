e1f786e1170fabe2507199554668fd363d8964ae
Ryan Waits <ryan.waits@gmail.com>
2016-11-07 21:17:30 -0600

    update class methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,13 @@
 class Task < ApplicationRecord
   has_many :parts
+
+  def parts_completed?
+    self.parts.where(done: false).empty?
+  end
+
+  def complete?
+    self.done == true
+  end
   
   def self.complete
     self.where(done: true)
@@ -9,16 +17,16 @@ class Task < ApplicationRecord
     self.where(in_progress: true)
   end
 
+  def self.most_recent
+    self.where(done: false).order('updated_at DESC')
+  end
+
   def self.latest
     self.where(done: false).order('updated_at DESC').limit(3)
   end
 
   def self.by_level
     self.where(done: false).order('level ASC')
-  end
-
-  def parts_completed?
-    self.parts.where(done: false).empty?
   end
 
 end
