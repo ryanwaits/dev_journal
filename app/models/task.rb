9bb4d52bcfe0857b6bc8a82cec1ae59662a3baf4
Ryan Waits <ryan.waits@gmail.com>
2016-11-08 00:18:48 -0600

    add .text_search method

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,6 +8,14 @@ class Task < ApplicationRecord
   def complete?
     self.done == true
   end
+
+  def self.text_search query
+    if query.present?
+      where("name ilike :q", q: "%#{query}%")
+    else
+      scoped
+    end
+  end
   
   def self.complete
     self.where(done: true)
