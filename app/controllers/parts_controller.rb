436fef80e4ff0ba7c1fd268503f10ae3417d1050
Ryan Waits <ryan.waits@gmail.com>
2016-11-07 18:27:27 -0600

    setup parts controller to show and update a part

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,4 +1,8 @@
 class PartsController < ApplicationController
+  before_action :find_part, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
 
   def new
     
@@ -12,10 +16,26 @@ class PartsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @part.update(part_params.merge!(done: true))
+    if @part.save
+      redirect_to @part.task
+    else
+      render :show
+    end
+  end
+
   private
 
+  def find_part
+    @part = Part.find(params[:id])
+  end
+
   def part_params
-    params.require(:part).permit(:description, :estimate, :actual, :reflection, :task_id)
+    params.require(:part).permit(:description, :estimate, :actual, :reflection, :priority, :done, :task_id)
   end
 
 end
