68776f2b1e94ac13bfa166965b4f7853ed51cb98
Ryan Waits <ryan.waits@gmail.com>
2016-11-09 10:43:52 -0600

    add routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'login' => 'sessions#new', as: 'login'
+  get 'logout' => 'sessions#destroy', as: 'logout'
+
   root 'welcome#index'
   get '/dashboard' => 'tasks#index', as: 'dashboard'
+
+  resources :users, only: [:new, :show, :create]
   resources :tasks, except: [:index] do
     resources :parts
   end
