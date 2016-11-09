fedf872179f17115dc916de3f387e107696596a2
Ryan Waits <ryan.waits@gmail.com>
2016-11-09 10:57:15 -0600

    add user column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109162456) do
+ActiveRecord::Schema.define(version: 20161109165312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20161109162456) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "in_progress", default: true
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +50,5 @@ ActiveRecord::Schema.define(version: 20161109162456) do
   end
 
   add_foreign_key "parts", "tasks"
+  add_foreign_key "tasks", "users"
 end
