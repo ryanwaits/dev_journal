135b9700c90f2b2498e8c0d95d94ebc9befebe3f
Ryan Waits <ryan.waits@gmail.com>
2016-11-10 10:54:06 -0600

    respond with all parts and only one part

--- app/controllers/api/v1/parts_controller.rb
+++ app/controllers/api/v1/parts_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::PartsController < Api::V1::BaseController
   def index
     task = Task.find(params[:task_id])
-    part = Part.find_by(task_id: task.id)
+    parts = Part.where(task_id: task.id)
+    
+    respond_with parts
+  end
+
+  def show
+    task = Task.find(params[:task_id])
+    part = Part.find(params[:id])
     
     respond_with part
   end
