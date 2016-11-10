b952124c7ff6e57be903ca1c94bd967071d4fb62
Ryan Waits <ryan.waits@gmail.com>
2016-11-10 11:28:09 -0600

    implement a callback that generates an auth token when a user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  before_create :generate_auth_token
+
   has_secure_password
   validates_presence_of :first_name, :last_name, :username, :email, :password
 
   has_many :tasks
+
+  def generate_auth_token
+    self.auth_token = SecureRandom.base64(64)
+  end
+
 end
