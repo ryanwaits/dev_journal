916a45f641a6d0e21c470b7cc68c9a5d3f50fd62
Ryan Waits <ryan.waits@gmail.com>
2016-11-10 10:37:00 -0600

    formatting

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::TasksController < Api::V1::BaseController
   def index
     tasks = Task.order('created_at Desc')
+    
     respond_with tasks
   end
 
   def show
     task = Task.find(params[:id])
+
     respond_with task
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def show
     user = User.find(params[:id])
+    
     respond_with user
   end
 end
