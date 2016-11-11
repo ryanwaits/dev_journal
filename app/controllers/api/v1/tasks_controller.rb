39a7a780af035f17afce0321230d894bc4957e5e
Ryan Waits <ryan.waits@gmail.com>
2016-11-11 11:18:12 -0600

    implement ability to create a task through api routes

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,4 +10,17 @@ class Api::V1::TasksController < Api::V1::BaseController
 
     respond_with task
   end
+
+  def create
+    task = @user.tasks.build(task_params)
+    if task.save
+      respond_with task
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :level)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'me' => 'users#show', as: 'me'
       resources :sessions, only: [:create, :destroy]
-      resources :tasks, only: [:index, :show]
+      resources :tasks, only: [:index, :show, :create]
       resources :tasks do
         resources :parts, only: [:index, :show]
       end
